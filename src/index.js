388a824b26a28021f42665f90d9f2e5d9f21f598
Hoon Lee <yl.hoony@gmail.com>
2018-06-01 17:35:12 -0700

    implemented

--- src/index.js
+++ src/index.js
@@ -1,1 +1,20 @@
-// Code Goes Here
+import React from 'react';
+import PropTypes from 'prop-types';
+import ReactDOM from 'react-dom';
+
+import Order from './components/Order'
+
+const scoopSelected = ['Blue', 'Red'];
+
+const orderDetail = {
+  customerName: "John Doe",
+  orderedAt: 1527893980
+}
+
+ReactDOM.render(
+  <Order 
+    scoops={scoopSelected}
+    orderInfo={orderDetail}
+  />,
+  document.getElementById('root')
+);
